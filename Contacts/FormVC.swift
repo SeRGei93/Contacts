8dd9f364d3304d0a146e27d3ba90d4705d5fe328
sergei <sb@slam.by>
2021-07-20 19:48:09 +0300

    Form VC

---
+++ Contacts/FormVC.swift
@@ -0,0 +1,27 @@
+//
+//  FormVC.swift
+//  Contacts
+//
+//  Created by Сергей Бушкевич on 20.07.21.
+//
+
+import UIKit
+
+class FormVC: ViewController {
+    
+    @IBOutlet weak var nameFiled: UITextField!
+    @IBOutlet weak var surNameField: UITextField!
+    @IBOutlet weak var ageField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func saveAction(_ sender: Any) {
+        
+    }
+    
+
+}
--- Contacts/ViewController.swift
+++ Contacts/ViewController.swift
@@ -8,12 +8,37 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
 }
 
+
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource{
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "image") else {
+            return UITableViewCell()
+        }
+        
+        return cell
+    }
+    
+
+    func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+        
+        
+    }
+
+}
